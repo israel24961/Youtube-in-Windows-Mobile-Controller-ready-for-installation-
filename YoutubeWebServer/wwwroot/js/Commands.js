e278a92bcf0041739de178c03102f8ced61313d4
israel <israel24961@hotmail.com>
2019-07-15 19:53:02 +0200

    v1.0

---
+++ YoutubeWebServer/wwwroot/js/Commands.js
@@ -0,0 +1,63 @@
+﻿var ButtonListNames = [
+    "Search",
+    "Next Result",
+    "Click",
+    "Previous Result",
+    "Center",
+    "Play/Pause",
+    "Next Video",
+    "Full Screen",
+    "Close Advertisement",
+    "Previous Page",
+    "Next Page",
+    "Close IE"
+];
+//Function for adding an array of elements to a list 
+//(in this case) ordered list
+var appendChildren = (list, Elements2Put) => {
+    let ar_listElement = [];
+    //fill with array
+
+    let max = Elements2Put.length;
+
+    for (let i = 0; i < max; i++) {
+        ar_listElement[i] = document.createElement('li');
+        ar_listElement[i].appendChild(Elements2Put[i]);
+    }
+    ar_listElement.forEach(element => {
+        list.appendChild(element);
+    });
+}
+
+var createButtons = () => {
+    let max = ButtonListNames.length;
+    let ElementsArray = [];
+    for (let index = 0; index < max; index++) {
+
+        let button = document.createElement('button');
+        if (ButtonListNames[index] == "Search") {
+            let i = index;
+            let temp_button = button;
+            temp_button.textContent = ButtonListNames[i];
+            temp_button.id = ButtonListNames[i];
+            temp_button.addEventListener('click', function () { Bucle('Search;' + document.getElementById('toSearchSTR').value + ';'); });
+            button = document.createElement('ul');
+            let text = document.createElement('textarea');
+            text.id = 'toSearchSTR';
+
+            button.appendChild(text);
+            button.appendChild(temp_button);
+            ElementsArray[i] = button;
+        }
+        else {
+            let i = index;
+            button.onclick = () => {
+                Bucle(ButtonListNames[i].toString());
+            };
+            button.textContent = ButtonListNames[i];
+            ElementsArray[i] = button;
+        }
+
+    }
+    return ElementsArray;
+};
---
+++ YoutubeWebServer/wwwroot/js/a_func.js
@@ -0,0 +1,81 @@
+﻿//States for the control
+const STATES = {
+    BeforeStart: 'bstart',
+    STARTED: 'started'
+}
+var state = STATES.BeforeStart;
+
+//List of Buttons
+var B_list = document.createElement("ol");
+B_list.style.display = 'none';
+B_list.id = 'MainOL'
+
+//Show status for the client
+var state_stringElement = document.getElementById("status");
+var updateState = (newState) => {
+    document.getElementById('status').textContent='NotConnected\n'
+};
+state_stringElement.textContent = "Not Connected\n";
+
+//function to append Li to the ordered List
+var socket;
+document.getElementById('connectIP').value = "192.168.1.136";
+
+var Bucle = (txt) => {
+    switch (state) {
+        case STATES.BeforeStart:
+            updateState('Not Connected');
+            if (txt == 'connect') {
+                try {
+
+                    document.getElementById("ConnectButton").disabled = true;
+
+                    //Communication
+                    let IP = document.getElementById('connectIP').value;
+                    socket = new WebSocket('ws://' + IP+'/ws');
+
+                    //Status for the client
+                    updateState("Connecting...\n");
+
+                    socket.addEventListener('open', function (event) {
+                        socket.send('Start');
+                        console.log("Connected to server");
+                        state_stringElement.textContent = "Connected";
+                        appendChildren(B_list, createButtons())
+                        B_list.style.display = 'block';
+                        document.getElementById('Second_OL').appendChild(B_list);
+                    });
+                    var restore2BeforeStart = (reason) => {
+                        state = STATES.BeforeStart;
+                        updateState("Not Connected:"+reason+'\n');
+                        document.getElementById("ConnectButton").disabled = false;
+                        //hide the buttons
+                        document.getElementById('Second_OL').style.display = 'none';
+                    }
+                    socket.addEventListener('error', (event) => {
+                        restore2BeforeStart('Error in connection')
+                    });
+                    socket.addEventListener('close', (ev) => {
+                        restore2BeforeStart('Connection Closed')
+                    });
+                    
+                    //next State
+                    state = STATES.STARTED;
+                } catch (error) {
+                    console.log(error);
+                    document.getElementById("ConnectButton").disabled = false;
+                }
+            }
+            break;
+        case STATES.STARTED:
+
+            socket.send(txt);
+
+            state = STATES.STARTED;
+            break;
+        default:
+            break;
+    }
+}
+
+Bucle();
